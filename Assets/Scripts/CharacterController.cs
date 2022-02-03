88f628f35bc33f7a71bdbd7d776c2ba7b3b68724
SaymanS <saymans@bk.ru>
2022-02-03 16:04:50 +0300

    Скрипт передвижения игрока
    Добавил возможность передвижения для игрока.
    
    Так же создал примитивную модель пистолета.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,14 +7,9 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     float _characterSpeed = 10;
 
-    [SerializeField]
-    BoxCollider _colider;
-
     private void Awake()
     {
-        //_colider = GetComponent<BoxCollider>();
-
-        TryGetComponent<BoxCollider>(out _colider);
+        
     }
 
     // Start is called before the first frame update
@@ -26,5 +21,6 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * _characterSpeed);
     }
 }
