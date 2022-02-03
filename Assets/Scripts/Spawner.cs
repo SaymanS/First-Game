8319dbf2ac7f642b9d5651f6de8dd02d1223dbf2
SaymanS <saymans@bk.ru>
2022-02-03 17:01:06 +0300

    Спаунер врагов
    Добавил спаунер врагов, так же разместил точки спауна.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _generatedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_generatedObject,transform.position,Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
