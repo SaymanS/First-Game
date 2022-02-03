93bcd9412576685d228ed874aeb0a99e2df66e7a
SaymanS <saymans@bk.ru>
2022-02-03 15:09:40 +0300

    Создан первый уровень и персонаж. Стены и двери.
    commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    float _characterSpeed = 10;
+
+    [SerializeField]
+    BoxCollider _colider;
+
+    private void Awake()
+    {
+        //_colider = GetComponent<BoxCollider>();
+
+        TryGetComponent<BoxCollider>(out _colider);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
